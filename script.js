5df1d8610161b1b65be9b434b827989a4bcacca9
nasir ali <anasir514@gmail.com>
2022-08-10 18:33:28 +0100

    Update script.js

--- script.js
+++ script.js
@@ -66,31 +66,37 @@ function darkModeOff() {
                                    //   end  head carosol Script 
 
 
+ //     Get To TOP button 
 
-//     Get To TOP button 
 
-        //Get the button
-        var mybutton = document.getElementById("myBtn");
-        
-        // When the user scrolls down 20px from the top of the document, show the button
-        window.onscroll = function() {scrollFunction()};
-        
-        function scrollFunction() {
-          if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-            mybutton.style.display = "block";
-          } else {
-            mybutton.style.display = "none";
-          }
-        }
-        
-        // When the user clicks on the button, scroll to the top of the document
-        function topFunction() {
-          document.body.scrollTop = 0;
-          document.documentElement.scrollTop = 0;
-        }
-       
+//Get the button
+let mybutton = document.getElementById("btn-back-to-top");
 
-                                   //   end  Get To TOP button
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 20 ||
+    document.documentElement.scrollTop > 20
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// When the user clicks on the button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+
+            //   end  Get To TOP button  
 
 
 
